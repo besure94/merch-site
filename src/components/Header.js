99d7abdd9b7767a0a40cee2dfea88f6a5fc4f2fb
Brian Scherner <brianscherner@gmail.com>
2024-03-13 15:43:22 -0700

    Add Header component to application.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="app-header">
+      <h1>Band Merch Site</h1>
+      <h3>Support your favorite band and check out their merch!</h3>
+    </div>
+  );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './components/index.css';
+import App from './components/App';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
