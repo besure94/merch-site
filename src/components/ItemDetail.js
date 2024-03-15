4134904f1547f8106d815cd98f9f032f2f639111
Brian Scherner <brianscherner@gmail.com>
2024-03-15 14:51:46 -0700

    Add functionality to Delete an Item.

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ItemDetail(props) {
-  const { item } = props;
+  const { item, onClickingDelete } = props;
 
   return (
     <React.Fragment>
@@ -12,13 +12,15 @@ function ItemDetail(props) {
       <h4>Color: {item.color}</h4>
       <h4>Size: {item.size}</h4>
       <h4>Quantity: {item.quantity}</h4>
+      <button onClick={() => onClickingDelete(item.id)}>Delete Item</button>
       <hr/>
     </React.Fragment>
   );
 }
 
 ItemDetail.propTypes = {
-  item: PropTypes.object
+  item: PropTypes.object,
+  onClickingDelete: PropTypes.func
 };
 
 export default ItemDetail;
