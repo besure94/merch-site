7e82d7476d7884324166dbf54b82da07544bd107
Brian Scherner <brianscherner@gmail.com>
2024-03-25 10:53:20 -0700

    Write passing test for a reducer adding an item to the item list.

--- src/__tests__/reducers/merch-site-reducer.test.js
+++ src/__tests__/reducers/merch-site-reducer.test.js
@@ -1,7 +1,42 @@
 import merchSiteReducer from '../../reducers/merch-site-reducer';
 
 describe('merchSiteReducer', () => {
+
+  let action;
+  const itemData = {
+    name: 'Rose and skull hoodie',
+    description: 'Black hoodie with red and white design.',
+    color: 'Black',
+    size: 'Large',
+    quantity: 5,
+    id: 1
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(merchSiteReducer({}, {type: null})).toEqual({});
+  });
+
+  test('Should successfully add new item to mainItemList', () => {
+    const { name, description, color, size, quantity, id } = itemData;
+    action = {
+      type: 'ADD_ITEM',
+      name: name,
+      description: description,
+      color: color,
+      size: size,
+      quantity: quantity,
+      id: id
+    };
+
+    expect(merchSiteReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        description: description,
+        color: color,
+        size: size,
+        quantity: quantity,
+        id: id
+      }
+    });
   });
 });
--- src/reducers/merch-site-reducer.js
+++ src/reducers/merch-site-reducer.js
@@ -1,5 +1,20 @@
 const reducer = (state = {}, action) => {
-  return state;
+  const { name, description, color, size, quantity, id } = action;
+  switch (action.type) {
+    case 'ADD_ITEM':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          description: description,
+          color: color,
+          size: size,
+          quantity: quantity,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 };
 
 export default reducer;
