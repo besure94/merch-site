cc7447fb93bfa0cb1d7b82e9f7944bef6ed51f5e
Brian Scherner <brianscherner@gmail.com>
2024-09-05 10:03:10 -0700

    Assign admin role via cloud function.

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,27 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+admin.initializeApp();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.assignAdminRole = functions.https.onCall((data, context) => {
+
+  if (context.auth.token.admin !== true) {
+    throw new functions.https.HttpsError('permission-denied', 'Only admins can assign roles.');
+  }
+
+  const newAdminUid = data.uid;
+
+  return admin.auth().setCustomUserClaims(newAdminUid, { admin: true }).then(() => {
+    return admin.firestore().collection('users').doc(newAdminUid).update({
+      role: 'admin'
+    }).then(() => {
+      return { message: `Success! User ${newAdminUid} has been granted admin rights.` };
+    });
+  }).catch(error => {
+    throw new functions.https.HttpsError('internal-error', error.message);
+  });
+
+});
